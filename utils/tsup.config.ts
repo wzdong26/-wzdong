9bd92dbdde4e05a7722d947de71b9959eaf41431
wzdong <wzdong.26@qq.com>
2022-12-26 22:43:02 +0800

    adjust build dir

--- utils/tsup.config.ts
+++ utils/tsup.config.ts
@@ -1,25 +1,27 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig((options) => ({
-    entry: ['src/**/**'],
-    format: ['cjs', 'iife'],
-    outDir: './lib',
-    outExtension({ format }) {
-        switch(format) {
-            case 'iife':
-                return {js: '.min.js'}
-            case 'cjs':
-                return {js: '.cjs'}
-            case 'esm':
-                return {js: '.mjs'}
-        }
-    },
-    globalName: 'utils',
-    splitting: true,
-    clean: true,
-    target: ['es6'],
-    minify: !options.watch,
-    treeshake: true,
-    dts: !options.watch,
-    metafile: !options.watch,
-}));
+export default defineConfig((options) => {
+    const { minify, watch } = options;
+    return {
+        entry: [`./src/${minify ? 'index.ts' : '*'}`],
+        outDir: `./${minify ? 'dist' : 'lib'}`,
+        format: minify ? ['cjs', 'iife'] : ['cjs'],
+        outExtension({ format, options }) {
+            switch (format) {
+                case 'iife':
+                    options.globalName = 'utils';
+                    return { js: '.min.js' };
+                case 'cjs':
+                    return { js: '.js' };
+                case 'esm':
+                    return { js: '.mjs' };
+            }
+        },
+        splitting: true,
+        clean: true,
+        target: ['es6'],
+        treeshake: true,
+        sourcemap: minify,
+        dts: !watch && !minify,
+    };
+});
