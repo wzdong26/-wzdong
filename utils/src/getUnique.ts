76c37c9ad39149a8c49541c1feaa4b70a8312ec2
wzdong <wzdong.26@qq.com>
2022-12-28 18:28:38 +0800

    add tsconfig target

--- utils/src/getUnique.ts
+++ utils/src/getUnique.ts
@@ -16,12 +16,12 @@
  * @param p 工厂函数 fn 的原始入参
  * @return R 工厂函数 fn 返回的对象
  */
-export const getUnique = <K extends any, P extends any[], R extends Exclude<any, undefined | null | void>>(
+export const getUnique = <K extends any, P extends any[], R extends Exclude<NonNullable<any>, void>>(
     fn: (...p: P) => R,
     set?: (key: K, rst: R) => void
 ) => {
     const results = new Map<K, R>();
-    return function (this: any, key: K, ...p: P) {
+    return function <T = any>(this: T, key: K, ...p: P) {
         let rst = results.get(key);
         return (
             rst ??
@@ -36,15 +36,43 @@ export const getUnique = <K extends any, P extends any[], R extends Exclude<any,
 };
 
 /**
+ * newUnique
+ * @param newFn 创建对象的构造函数
+ * @param set 设置函数，为创建的对象添加唯一标识属性，默认不设置
+ * ---
+ * @returns function (key: K, ...p: P) => R
+ * @param key 唯一标识值, 建议用 symbol
+ * @param p 构造函数 newFn 的原始入参
+ * @return R 构造函数 newFn 返回的对象
+ */
+export const newUnique = <K extends any, P extends any[], R extends Exclude<any, undefined | null | void>>(
+    newFn: new (...p: P) => R,
+    set?: (key: K, rst: R) => void
+) => getUnique<K, P, R>((...p) => new newFn(...p), set);
+
+/**
  * getSingle: 将需要 key 传入的 {@link getUnique} 生成的方法变为不需要 key的方法
  * @param fn 创建对象的工厂函数
  * ---
- * @returns function {@link GetSingleReturn}
+ * @returns function (...p: P) => R
+ * @param p 工厂函数 fn 的原始入参
+ * @return R 工厂函数 fn 返回的对象
  */
-export const getSingle = <P extends any[], R extends Exclude<any, undefined | null | void>>(fn: (...p: P) => R) => {
+export const getSingle = <P extends any[], R extends Exclude<NonNullable<any>, void>>(fn: (...p: P) => R) => {
     const key = Symbol();
     const uniFn = getUnique(fn);
-    return function (this: any, ...p: P) {
+    return function <T = any>(this: T, ...p: P) {
         return uniFn.apply(this, [key, ...p]);
     };
 };
+
+/**
+ * getSingle: 将需要 key 传入的 {@link getUnique} 生成的方法变为不需要 key的方法
+ * @param newFn 创建对象的工厂函数
+ * ---
+ * @returns function (...p: P) => R
+ * @param p 构造函数 newFn 的原始入参
+ * @return R 构造函数 newFn 返回的对象
+ */
+export const newSingle = <P extends any[], R extends Exclude<any, undefined | null | void>>(newFn: new (...p: P) => R) =>
+    getSingle<P, R>((...p) => new newFn(...p));
