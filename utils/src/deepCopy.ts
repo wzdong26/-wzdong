bbf9e23d82a6b6df21ed880c347456595c1fa099
wzdong <wzdong.26@qq.com>
2023-03-11 14:44:54 +0800

    utils update `deepCopy`

--- utils/src/deepCopy.ts
+++ utils/src/deepCopy.ts
@@ -4,19 +4,51 @@
  * @author wzdong
  */
 
-const deepCopy = (obj: any) => {
-    // 浅拷贝原型链
-    const copiedObj = Object.create(Object.getPrototypeOf(obj));
-    // 获取拷贝对象 obj 的 PropertyDescriptors
+// 拷贝常规对象、数组(不包含Function、Map、Set)
+const deepCopySimplify = (obj: unknown, visited = new WeakMap()) => {
+    let copied = obj;
+    
+    // 不是引用型，直接返回
+    if (!(obj instanceof Object)) return copied;
+
+    // 循环引用, 则返回 weakmap 中映射的对象
+    if (visited.has(obj)) return (copied = visited.get(obj));
+
+    // 创建引用对象副本, 并存入 visited WeakMap 中
+    copied = Object.create(Object.getPrototypeOf(obj));
+    visited.set(obj, copied);
+
+    // 核心
     const objDescriptors = Object.getOwnPropertyDescriptors(obj);
-    // Reflect.ownKeys 获取 PropertyDescriptors 的键数组, Reflect.ownKeys 优势在于可获取对象自身的所有属性
     for (const k of Reflect.ownKeys(objDescriptors)) {
         if (objDescriptors[k].value instanceof Object) {
-            objDescriptors[k].value = deepCopy(obj[k]);
+            objDescriptors[k].value = deepCopy(obj[k], visited);
         }
     }
-    Object.defineProperties(copiedObj, objDescriptors);
-    return copiedObj;
+    Object.defineProperties(copied, objDescriptors);
+    return copied;
+};
+
+const deepCopy = (obj: unknown, visited = new WeakMap()) => {
+    let copied = obj;
+
+    // 函数
+    if (obj instanceof Function) {
+        let fnStr = obj.toString();
+        if (fnStr.indexOf('(') > 0) {
+            fnStr = 'function' + fnStr.slice(fnStr.indexOf('('));
+        }
+        return (copied = new Function('return ' + fnStr)());
+    }
+
+    // Map
+    if (obj instanceof Map) return (copied = new Map(deepCopySimplify([...obj], visited)));
+
+    // Set
+    if (obj instanceof Set) return (copied = new Set(deepCopySimplify([...obj], visited)));
+
+    // 常规对象、数组、非引用类型
+    return (copied = deepCopySimplify(obj, visited));
 };
 
 export default deepCopy;
